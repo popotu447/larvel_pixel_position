213ac350dbc5a34dc1fbe401decfa94e3cdfa87e
popotu447 <my_sql@op.pl>
2025-04-24 11:49:53 +0200

    2025_04_24_config_files

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,7 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            buildDirectory: 'build'
         }),
     ],
     build: {
